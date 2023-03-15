65ab7ba103788b0c1b935527e1683a51d4bb12aa
MatheusSchwerz <matheusschwert@gmail.com>
2023-03-15 08:44:34 -0300

    new server

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0d50e5e845c552faf"
+  instance_type = "t2.micro"
+  key_name = "iac-alura"
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
