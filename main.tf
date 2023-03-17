8c1a0cd6c827099b907559752b63fd88e7b1a1ed
MatheusSchwerz <matheusschwert@gmail.com>
2023-03-17 19:35:54 -0300

    first project

--- main.tf
+++ main.tf
@@ -18,6 +18,6 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
   key_name = "iac-alura"
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "Terraform Ansible"
   }
 }
